f5a3dbfd2dd487084e4f1d9476aad92d76736c01
Trevor Tang <tangt07@gmail.com>
2015-08-05 20:00:47 -0500

    initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	public KeyCode left;
+	public KeyCode right;
+	public KeyCode jump;
+	public KeyCode projectile;
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void Jump(){
+
+	}
+	void Left(){
+
+	}
+	void Right(){
+
+	}
+	void Projectile(){
+
+	}
+
+
+}
