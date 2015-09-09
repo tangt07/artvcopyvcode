f09be193082aa44a5330f73785aeac62ef2ee239
Trevor Tang <tangt07@gmail.com>
2015-09-09 16:44:15 -0500

    character select

--- Assets/Scripts/Player1Select.cs
+++ Assets/Scripts/Player1Select.cs
@@ -9,11 +9,10 @@ public class Player1Select : MonoBehaviour {
 	public Button craigbutton;
 	public Button amybutton;
 	public Button willbutton;
-	public Button selectbutton;
 	public Button killerbutton;
-	public Image killercanvas;
-	public Text confirm;
-	public Text selectbuttontext;
+
+	public Text charName;
+	public Text charInfo;
 	private Dictionary<string,Button> _dicButtonByName = null;
 
 	private Dictionary<string,PlayerName> _dicPlayerNameByName = null;
@@ -39,9 +38,11 @@ public class Player1Select : MonoBehaviour {
 	}
 	// Use this for initialization
 	void Start () {
-		selectbuttontext = selectbutton.GetComponentInChildren<Text> ();
+		if (SceneManager.players == 0) {
+			SceneManager.players = 2;
+		}
 		if (SceneManager.players == 1) {
-			killercanvas.gameObject.SetActive(false);
+			killerbutton.gameObject.SetActive(false);
 		}
 	
 	}
@@ -51,36 +52,36 @@ public class Player1Select : MonoBehaviour {
 		if (EventSystem.current.currentSelectedGameObject == null) {
 			EventSystem.current.SetSelectedGameObject (craigbutton.gameObject);
 		}
-
-		if(EventSystem.current.currentSelectedGameObject != selectbutton.gameObject){
-			selectbutton.interactable = false;
-			selectbuttontext.text = "Select";
-			player1name = PlayerName.None;
-			confirm.enabled = false;
-
-
+		if (EventSystem.current.currentSelectedGameObject == craigbutton.gameObject) {
+			charName.text = "Craig the Copywriter";
+			charInfo.text ="Craig believes that anything and everything in this world is a story\nwaiting to be told -- and after a few beers, he's ready to tell them\nall. He can hypnotize with words, and craft another person's thoughts\nwith the right combination of storytelling and masterful facial hair.\nHis toolbox is simple, but powerful: 26 tiny instruments that know no\nboundaries and can transform dreams into reality.";
 		}
+		if (EventSystem.current.currentSelectedGameObject == amybutton.gameObject) {
+			charName.text = "Amy the Art Director";
+			charInfo.text ="An artist at heart, Amy the AD always dreamed of sharing her talents\nwith the world. But the life of a "+'"'+"starving artist"+'"'+" didn't suit her,\nso when the advertising world came calling, she traded her crayons\nand brushes for Adobe Suite and a Wacom tablet. Whoever thought\nthat Keynote couldn't be a canvas never saw Amy sculpt a presentation\ndeck like Michelangelo, either. She is a visually - driven idea builder,\ncapable of using her designer toolkit to create almost anything.";
+		}
+		if (EventSystem.current.currentSelectedGameObject == willbutton.gameObject) {
+			charName.text = "Will the Creative Technologist";
+			charInfo.text ="Will the CT stands at the cusp of new technology, using it freely and\nexpertly in his creative pursuits. This makes him something of a\nrenaissance man in the ad agency world, maneuvering seamlessly\nbetween 'creative' and 'coder' -- although he couldn't tell you how or\nwhy. His achilles heel is answering the question, 'What do you do?'\nBut otherwise, he's a fearless creator, constantly experimenting and\ndreaming of pioneering his own brand of 'new tech.'";
+		}
+		if (EventSystem.current.currentSelectedGameObject == killerbutton.gameObject) {
+			charName.text = "The Idea Killer";
+			charInfo.text ="The Idea Killer hides in the dark corners of the creative world,\nwaiting patiently for the perfect time to strike -- epiphanies. He\ngobbles them up and spits them back out, taking pleasure in the\nsoul-crushing disappointment of Creatives. Even great ideas don't\nstand a chance, as he can tear them down with little more than a\nconfused glance, or a "+'"'+"meh"+'"'+" expression. He reviles creativity, but\ndoesn't hesitate to use it himself -- to kill great ideas.";
+		}
+
+
 
 	}
 	public void SelectActivate(string name){
-		var navigation = selectbutton.navigation;
-		navigation.selectOnUp = _dicButtonByName[name];
-		selectbutton.navigation = navigation;
+
 		player1name = _dicPlayerNameByName[name];
 
+		PlaySelectedCharacter ();
+	
 
-		ModifyButton ();
-
-
-	}
-	void ModifyButton(){
-		
-		confirm.enabled = true;
-		selectbutton.interactable = true;
-		selectbuttontext.text = player1name + " Select";
-		EventSystem.current.SetSelectedGameObject (selectbutton.gameObject);
 
 	}
+
 	public void PlaySelectedCharacter(){
 		if (SceneManager.players == 1) {
 			Application.LoadLevel ("Game");
--- Assets/Scripts/Player2Select.cs
+++ Assets/Scripts/Player2Select.cs
@@ -9,96 +9,82 @@ public class Player2Select : MonoBehaviour {
 	public Button craigbutton;
 	public Button amybutton;
 	public Button willbutton;
-	public Button selectbutton;
 	public Button killerbutton;
-	public Text confirm;
-	public Text selectbuttontext;
-	private Dictionary<string,Button> _dicButtonByName = null;
-
-	private Dictionary<PlayerName,Button> _dicButtonByPName = null;
-
+	
+	public Text charName;
+	public Text charInfo;
+	private Dictionary<PlayerName,Button> _dicButtonByName = null;
+	
 	private Dictionary<string,PlayerName> _dicPlayerNameByName = null;
-
-
+	
+	
 	public static PlayerName player2name = PlayerName.None;
-
-
+	
+	
 	void Awake(){
-		_dicButtonByName = new Dictionary<string, Button> ();
-		_dicButtonByName.Add ("Craig", craigbutton);
-		_dicButtonByName.Add ("Amy", amybutton);
-		_dicButtonByName.Add ("Will", willbutton);
-		_dicButtonByName.Add ("Killer", killerbutton);
-
-		_dicButtonByPName = new Dictionary<PlayerName, Button> ();
-		_dicButtonByPName.Add (PlayerName.Craig, craigbutton);
-		_dicButtonByPName.Add (PlayerName.Amy, amybutton);
-		_dicButtonByPName.Add (PlayerName.Will, willbutton);
-		_dicButtonByPName.Add (PlayerName.Killer, killerbutton);
-
+		_dicButtonByName = new Dictionary<PlayerName, Button> ();
+		_dicButtonByName.Add (PlayerName.Craig, craigbutton);
+		_dicButtonByName.Add (PlayerName.Amy, amybutton);
+		_dicButtonByName.Add (PlayerName.Will, willbutton);
+		_dicButtonByName.Add (PlayerName.Killer, killerbutton);
+		
 		_dicPlayerNameByName = new Dictionary<string, PlayerName> ();
 		_dicPlayerNameByName.Add ("Craig", PlayerName.Craig);
 		_dicPlayerNameByName.Add ("Amy", PlayerName.Amy);
 		_dicPlayerNameByName.Add ("Will", PlayerName.Will);
 		_dicPlayerNameByName.Add ("Killer", PlayerName.Killer);
-
+		
 		
 	}
 	// Use this for initialization
 	void Start () {
+		if (SceneManager.players == 0) {
+			SceneManager.players = 2;
+		}
+		if (SceneManager.players == 1) {
+			killerbutton.gameObject.SetActive(false);
+		}
 		if (Player1Select.player1name == null || Player1Select.player1name == PlayerName.None) {
 			Player1Select.player1name = PlayerName.Craig;
 		}
-		_dicButtonByPName[Player1Select.player1name].interactable = false;
-
-		selectbuttontext = selectbutton.GetComponentInChildren<Text> ();
-	
+		_dicButtonByName[Player1Select.player1name].interactable = false;
+		
 	}
+	
 	// Update is called once per frame
 	void Update () {
 		if (EventSystem.current.currentSelectedGameObject == null) {
-			if(Player1Select.player1name != PlayerName.Craig){
-				EventSystem.current.SetSelectedGameObject (craigbutton.gameObject);
-			}else{
-				EventSystem.current.SetSelectedGameObject (amybutton.gameObject);
-			}
-		} 
-
-
-
-		if(EventSystem.current.currentSelectedGameObject != selectbutton.gameObject){
-			selectbutton.interactable = false;
-			selectbuttontext.text = "Select";
-			player2name = PlayerName.None;
-			confirm.enabled = false;
-
-
+			EventSystem.current.SetSelectedGameObject (craigbutton.gameObject);
 		}
-
+		if (EventSystem.current.currentSelectedGameObject == craigbutton.gameObject) {
+			charName.text = "Craig the Copywriter";
+			charInfo.text ="Craig believes that anything and everything in this world is a story\nwaiting to be told -- and after a few beers, he's ready to tell them\nall. He can hypnotize with words, and craft another person's thoughts\nwith the right combination of storytelling and masterful facial hair.\nHis toolbox is simple, but powerful: 26 tiny instruments that know no\nboundaries and can transform dreams into reality.";
+		}
+		if (EventSystem.current.currentSelectedGameObject == amybutton.gameObject) {
+			charName.text = "Amy the Art Director";
+			charInfo.text ="An artist at heart, Amy the AD always dreamed of sharing her talents\nwith the world. But the life of a "+'"'+"starving artist"+'"'+" didn't suit her,\nso when the advertising world came calling, she traded her crayons\nand brushes for Adobe Suite and a Wacom tablet. Whoever thought\nthat Keynote couldn't be a canvas never saw Amy sculpt a presentation\ndeck like Michelangelo, either. She is a visually - driven idea builder,\ncapable of using her designer toolkit to create almost anything.";
+		}
+		if (EventSystem.current.currentSelectedGameObject == willbutton.gameObject) {
+			charName.text = "Will the Creative Technologist";
+			charInfo.text ="Will the CT stands at the cusp of new technology, using it freely and\nexpertly in his creative pursuits. This makes him something of a\nrenaissance man in the ad agency world, maneuvering seamlessly\nbetween 'creative' and 'coder' -- although he couldn't tell you how or\nwhy. His achilles heel is answering the question, 'What do you do?'\nBut otherwise, he's a fearless creator, constantly experimenting and\ndreaming of pioneering his own brand of 'new tech.'";
+		}
+		if (EventSystem.current.currentSelectedGameObject == killerbutton.gameObject) {
+			charName.text = "The Idea Killer";
+			charInfo.text ="The Idea Killer hides in the dark corners of the creative world,\nwaiting patiently for the perfect time to strike -- epiphanies. He\ngobbles them up and spits them back out, taking pleasure in the\nsoul-crushing disappointment of Creatives. Even great ideas don't\nstand a chance, as he can tear them down with little more than a\nconfused glance, or a "+'"'+"meh"+'"'+" expression. He reviles creativity, but\ndoesn't hesitate to use it himself -- to kill great ideas.";
+		}
+		
+		
+		
 	}
 	public void SelectActivate(string name){
-		var navigation = selectbutton.navigation;
-		navigation.selectOnUp = _dicButtonByName[name];
-		selectbutton.navigation = navigation;
-		player2name = _dicPlayerNameByName[name];
-
-
-		ModifyButton ();
-
-
-	}
-
-
-	void ModifyButton(){
 		
-		confirm.enabled = true;
-		selectbutton.interactable = true;
-		selectbuttontext.text = player2name + " Select";
-		EventSystem.current.SetSelectedGameObject (selectbutton.gameObject);
-
+		player2name = _dicPlayerNameByName[name];
+		
+		Application.LoadLevel ("Game");
+		
+		
+		
 	}
-	public void PlaySelectedCharacter(){
-		Application.LoadLevel("Game");
+	
 
-	}
 }
